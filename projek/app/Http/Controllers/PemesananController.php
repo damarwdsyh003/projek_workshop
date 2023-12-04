4a5545234d5365e2230f3a5d46c4d73077c3e80d
damarwdsyh003 <53498693+damarwdsyh003@users.noreply.github.com>
2023-12-04 13:06:48 +0700

    tampilan form-pemesanan

--- projek/app/Http/Controllers/PemesananController.php
+++ projek/app/Http/Controllers/PemesananController.php
@@ -9,41 +9,60 @@ use App\Models\Pemesanan; // Pastikan Anda memiliki model Pemesanan yang sesuai
 
 class PemesananController extends Controller
 {
-    public function index()
-    {
-        // Ambil daftar meja yang tersedia
-        $mejaTersedia = Meja::where('status', 'kosong')->get();
+    // public function index()
+    // {
+    //     // Ambil daftar meja yang tersedia
+    //     $mejaTersedia = Meja::where('status', 'kosong')->get();
 
-        // Ambil daftar paket yang tersedia
-        $paketTersedia = Paket::all();
+    //     // Ambil daftar paket yang tersedia
+    //     $paketTersedia = Paket::all();
 
-        return view('pemesanan.index', compact('mejaTersedia', 'paketTersedia'));
-    }
+    //     return view('pemesanan.index', compact('mejaTersedia', 'paketTersedia'));
+    // }
 
-    public function pesan(Request $request)
-    {
-        // Validasi data yang diinput oleh pengguna
-        $this->validate($request, [
-            'meja_id' => 'required',
-            'paket_id' => 'required',
-            // tambahkan validasi lainnya sesuai kebutuhan
-        ]);
+    // public function pesan(Request $request)
+    // {
+    //     // Validasi data yang diinput oleh pengguna
+    //     $this->validate($request, [
+    //         'meja_id' => 'required',
+    //         'paket_id' => 'required',
+    //         // tambahkan validasi lainnya sesuai kebutuhan
+    //     ]);
 
-        // Buat pemesanan baru
-        $pemesanan = new Pemesanan;
-        $pemesanan->meja_id = $request->meja_id;
-        $pemesanan->paket_id = $request->paket_id;
-        // Isi data pemesanan lainnya sesuai kebutuhan
+    //     // Buat pemesanan baru
+    //     $pemesanan = new Pemesanan;
+    //     $pemesanan->meja_id = $request->meja_id;
+    //     $pemesanan->paket_id = $request->paket_id;
+    //     // Isi data pemesanan lainnya sesuai kebutuhan
 
-        // Simpan pemesanan
-        $pemesanan->save();
+    //     // Simpan pemesanan
+    //     $pemesanan->save();
 
-        // Update status meja menjadi "dipesan"
-        $meja = Meja::find($request->meja_id);
-        $meja->status = 'dipesan';
-        $meja->save();
+    //     // Update status meja menjadi "dipesan"
+    //     $meja = Meja::find($request->meja_id);
+    //     $meja->status = 'dipesan';
+    //     $meja->save();
 
-        // Redirect ke halaman pemesanan sukses atau lainnya
-        return redirect()->route('pemesanan.index')->with('success', 'Pemesanan berhasil dilakukan.');
-    }
+    //     // Redirect ke halaman pemesanan sukses atau lainnya
+    //     return redirect()->route('pemesanan.index')->with('success', 'Pemesanan berhasil dilakukan.');
+    // }
+    public function store(Request $request)
+{
+    // Validate the inputs
+    $request->validate([
+        'package' => 'required',
+        'table_number' => 'required|numeric|min:1|max:10',
+        'order_date' => 'required|date',
+    ]);
+
+    // Create a new order
+    $order = new Pemesanan();
+    $order->package = $request->package;
+    $order->table_number = $request->table_number;
+    $order->order_date = $request->order_date;
+    $order->save();
+
+    // Redirect the user to a success page
+    return redirect()->route('success');
+}
 }
---
+++ projek/resources/views/reservation.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Internet Cafe</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/pemesanan.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 579 Cyborg Gaming
+
+https://templatemo.com/tm-579-cyborg-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/img/inca.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Search End ***** -->
+                    {{-- <div class="search-input">
+                      <form id="search" action="#">
+                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                        <i class="fa fa-search"></i>
+                      </form>
+                    </div> --}}
+                    <!-- ***** Search End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="{{ route('home')}}">Home</a></li>
+                        <li><a href="{{ route('about')}}">About</a></li>
+                        <li><a href="{{ route('facilities')}}">Facilities</a></li>
+                        <li><a href="{{ route('reservation')}}" class="active">Reservation</a></li>
+                        <li><a href="{{ route('profile')}}">Profile <img src="assets/img/profile-header.jpg" alt=""></a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+          <!-- ***** Most Popular Start ***** -->
+          <div class="most-popular">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="heading-section">
+                  <h4><em>Make Your</em> Reservation</h4>
+                </div>
+                <form action="{{ url('store') }}" method="POST">
+                  @csrf
+          
+                  <label for="package">Package:</label>
+                  <select name="package" id="package" required>
+                      <option value="Package 1">Package 1</option>
+                      <option value="Package 2">Package 2</option>
+                      <option value="Package 3">Package 3</option>
+                  </select>
+          
+                  <br>
+          
+                  <label for="table_number">Table Number:</label>
+                  <input type="number" name="table_number" id="table_number" min="1" max="10" required>
+          
+                  <br>
+          
+                  <label for="order_date">Order Date:</label>
+                  <input type="date" name="order_date" id="order_date" required>
+          
+                  <br>
+          
+                  <input type="submit" value="Submit">
+              </form>
+          </body>
+          </html>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Most Popular End ***** -->
+
+          <!-- ***** Gaming Library Start ***** -->
+          {{-- <div class="gaming-library">
+            <div class="col-lg-12">
+              <div class="heading-section">
+                <h4><em>Your Gaming</em> Library</h4>
+              </div>
+              <div class="item">
+                <ul>
+                  <li><img src="assets/img/game-01.jpg" alt="" class="templatemo-item"></li>
+                  <li><h4>Dota 2</h4><span>Sandbox</span></li>
+                  <li><h4>Date Added</h4><span>24/08/2036</span></li>
+                  <li><h4>Hours Played</h4><span>634 H 22 Mins</span></li>
+                  <li><h4>Currently</h4><span>Downloaded</span></li>
+                  <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>
+                </ul>
+              </div>
+              <div class="item">
+                <ul>
+                  <li><img src="assets/img/game-02.jpg" alt="" class="templatemo-item"></li>
+                  <li><h4>Fortnite</h4><span>Sandbox</span></li>
+                  <li><h4>Date Added</h4><span>22/06/2036</span></li>
+                  <li><h4>Hours Played</h4><span>740 H 52 Mins</span></li>
+                  <li><h4>Currently</h4><span>Downloaded</span></li>
+                  <li><div class="main-border-button"><a href="#">Donwload</a></div></li>
+                </ul>
+              </div>
+              <div class="item last-item">
+                <ul>
+                  <li><img src="assets/img/game-03.jpg" alt="" class="templatemo-item"></li>
+                  <li><h4>CS-GO</h4><span>Sandbox</span></li>
+                  <li><h4>Date Added</h4><span>21/04/2036</span></li>
+                  <li><h4>Hours Played</h4><span>892 H 14 Mins</span></li>
+                  <li><h4>Currently</h4><span>Downloaded</span></li>
+                  <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="main-button">
+                <a href="profile.html">View Your Library</a>
+              </div>
+            </div>
+          </div> --}}
+          <!-- ***** Gaming Library End ***** -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {{-- <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved.
+
+          <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>  Distributed By <a href="https://themewagon.com" target="_blank" >ThemeWagon</a></p>
+        </div>
+      </div>
+    </div>
+  </footer> --}}
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
